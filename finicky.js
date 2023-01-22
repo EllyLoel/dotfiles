b401a744ff5399105943a258827837f4f8b7466e
Elly Loel <hello@ellyloel.com>
2023-01-22 23:17:23 +1100

    ✨ feat: adding finicky

---
+++ finicky.js
@@ -0,0 +1,19 @@
+// Use https://finicky-kickstart.now.sh to generate basic configuration
+// Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
+
+module.exports = {
+  defaultBrowser: "Arc",
+  rewrite: [
+    {
+      // Redirect all urls to use https
+      match: ({ url }) => url.protocol === "http",
+      url: { protocol: "https" },
+    },
+  ],
+  handlers: [
+    {
+      match: /^https?:\/\/localhost:.*$/,
+      browser: "Polypane",
+    },
+  ],
+};
