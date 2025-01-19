60dfc92d509a1bc3e6666e73dd5fb2183b2692e8
Elly Loel <hello@ellyloel.com>
2025-01-19 20:12:40 +1100

    Update finicky config

--- finicky.js
+++ finicky.js
@@ -2,19 +2,5 @@
 // Learn more about configuration options: https://github.com/johnste/finicky/wiki/Configuration
 
 module.exports = {
-  defaultBrowser: "Arc",
-  rewrite: [
-    {
-      // Redirect all urls to use https
-      match: ({ url }) =>
-        url.protocol === "http" && !url.host.includes("localhost"),
-      url: { protocol: "https" },
-    },
-  ],
-  handlers: [
-    {
-      match: /^https?:\/\/localhost:.*$/,
-      browser: "Polypane",
-    },
-  ],
+  defaultBrowser: "Firefox",
 };
